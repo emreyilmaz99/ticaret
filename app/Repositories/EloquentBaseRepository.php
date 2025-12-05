36a6e3425a7f6a52da41015bef0cf2cd632be135
Emre Yılmaz <eem654re@gmail.com>
2025-12-05 17:40:54 +0300

    sepet fixed
    sepet

--- app/Repositories/EloquentBaseRepository.php
+++ app/Repositories/EloquentBaseRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Repositories\Interfaces\BaseRepositoryInterface;
 use Illuminate\Database\Eloquent\Model;
 
 abstract class EloquentBaseRepository implements BaseRepositoryInterface
