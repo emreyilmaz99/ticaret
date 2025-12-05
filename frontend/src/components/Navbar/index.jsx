04ceb787fdc5e04db2dfdc8d11172554ef9b8e47
ysrmhm <tyasarmuhammed@gmail.com>
2025-12-05 10:37:33 +0300

    sdas
    asdasd

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FaUser, FaShoppingBag, FaHeart, FaSignOutAlt } from 'react-icons/fa';
 import AddressModal from '../AddressModal';
